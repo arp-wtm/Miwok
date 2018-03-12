faa73d8ebec60e6523c9d65c0e4ab0414ee6c061
Antonella Rotondo <a.rotondo.me@gmail.com>
2018-03-12 12:51:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/NumbersClickListener.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.miwok;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Antonella on 06/02/18.
+ */
+
+public class NumbersClickListener implements View.OnClickListener {
+
+    // override the abstract method of the interface OnClickListener
+
+    @Override
+    public void onClick(View view){
+        Toast.makeText(view.getContext(),
+                "Open the list of numbers",Toast.LENGTH_LONG).show();
+    }
+
+}

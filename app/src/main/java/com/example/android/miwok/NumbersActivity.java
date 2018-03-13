b10f0d3f8aefa604b551c0c0c86d606cd00157cd
Antonella Rotondo <a.rotondo.me@gmail.com>
2018-03-13 12:36:03 +0100

    changed array structure to ArrayList in NumbersActivity.java for store words "one","two",..."ten". Upadate Log msg.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -16,9 +16,11 @@
 
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -28,21 +30,33 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // Create an array of words from one until ten
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        // Create an arrayList of words from one until ten
 
-        Log.v("NumbersActivity", "word at index 0:" + words[0]);
-        Log.v("NumbersActivity", "word at index 1:" + words[1]);
+        ArrayList<String> words = new ArrayList<>();
+        words.add(0,"one");
+        words.add(1, "two");
+        words.add(2, "three");
+        words.add(3, "four");
+        words.add(4, "five");
+        words.add(5, "six");
+        words.add(6, "seven");
+        words.add(7, "eight");
+        words.add(8, "nine");
+        words.add(9, "ten");
+
+        // print out each element of the words array to the Android logs
+        // use log.v with emulator, log.i with physical device
+
+        Log.i("NumbersActivity", "word at index 0:" + words.get(0));
+        Log.i("NumbersActivity", "word at index 1:" + words.get(1));
+        Log.i("NumbersActivity", "word at index 2:" + words.get(2));
+        Log.i("NumbersActivity", "word at index 3:" + words.get(3));
+        Log.i("NumbersActivity", "word at index 4:" + words.get(4));
+        Log.i("NumbersActivity", "word at index 5:" + words.get(5));
+        Log.i("NumbersActivity", "word at index 6:" + words.get(6));
+        Log.i("NumbersActivity", "word at index 7:" + words.get(7));
+        Log.i("NumbersActivity", "word at index 8:" + words.get(8));
+        Log.i("NumbersActivity", "word at index 9:" + words.get(9));
 
 
     }

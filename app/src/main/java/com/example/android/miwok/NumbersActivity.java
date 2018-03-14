9eaaeefbe1f7525f4db6c1159019daddc3647ef8
Antonella Rotondo <a.rotondo.me@gmail.com>
2018-03-14 18:38:16 +0100

    used ListView with ArrayAdapter to display list of words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,7 +18,9 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,35 +46,28 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(7, "eight");
         words.add(8, "nine");
         words.add(9, "ten");
+        words.add(10, "eleven");
 
-        // Find the external layout that contains the column of words
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
 
-        LinearLayout rootLayout = (LinearLayout) findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        // variable for count the numbers of cicles and the right position
-        // of the elements of ArrayList
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
 
+        ListView listView = (ListView) findViewById(R.id.list);
 
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
 
-
-        // loop until the last position
-
-        for (int index = 0;index < words.size(); index++) {
-
-            // create a texView at each cycle
-
-            TextView wordView = new TextView(this);
-
-            // set the text with that one stored at position referred by index
-            wordView.setText(words.get(index));
-
-
-            //add another child textView
-            if (rootLayout != null) {
-                rootLayout.addView(wordView);
-            }
-
-        }
+        listView.setAdapter(itemsAdapter);
     }
 
 
